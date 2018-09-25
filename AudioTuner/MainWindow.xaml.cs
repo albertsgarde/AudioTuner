fa5bbedf51eabe34410fa355859ff14668b3fde4
albert <albertsgarde@gmail.com>
2018-09-25 11:24:41 +0200

    _

--- AudioTuner/MainWindow.xaml.cs
+++ AudioTuner/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NAudio.Wave;
+using Stuff.DSP;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,22 @@ namespace AudioTuner
         public MainWindow()
         {
             InitializeComponent();
+            using (var audioFile = new AudioFileReader("C:/Users/alber/Documents/Synth/wav/sine_440.wav"))
+            {
+                var stream = audioFile.Take(new TimeSpan(0, 0, 3));
+
+                var bufferLength = 100000;
+                float[] buffer = new float[bufferLength];
+                stream.Read(buffer, 0, bufferLength);
+
+                var rdft = new RunningDFT(2000);
+                var f_s = Math.PI * 2 / 44100;
+                var f = 970;
+                for (int i = 0; i < 4000; ++i)
+                    rdft.NewSample((float)Math.Sin(i*f_s*f));
+
+                Frequency.Text = "" + rdft.Fundamental() * 44100;
+            }
         }
     }
 }
